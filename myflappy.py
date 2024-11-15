48f13a58546498ad14c52c00178c40c005dd4e30
Ayaan Nasim <ayaannasim@Ayaans-MacBook-Air.local>
2024-11-15 11:29:18 +0000

    Added jumping

--- myflappy.py
+++ myflappy.py
@@ -3,28 +3,47 @@ import tkinter as tk
 
 root=tk.Tk()
 root.title("Flappy Bird")
-width, height=600,400
-root.geometry(f"{width}x{height}")
+WIDTH, HEIGHT=600,400
+root.geometry(f"{WIDTH}x{HEIGHT}")
 
 background_image=tk.PhotoImage(file="background3.png")
 
-canvas=tk.Canvas(root,width=width,height=height)
+canvas=tk.Canvas(root,width=WIDTH,height=HEIGHT)
 canvas.pack(fill="both", expand=True)
 
 canvas.create_image(0, 0, image=background_image, anchor="nw")
 
-# bird_image=tk.PhotoImage(file="bird3.png")
-# bird_x, bird_y = 50, 250
-# bird = canvas.create_image(bird_x, bird_y, image=bird_image, anchor="nw")
-
 bird_size = 30
 bird_x = 50
-bird_y = height // 2
+bird_y = HEIGHT // 2
 bird_speed_y = 0
 gravity = 1
-jump_strength = -12
+jump_strength = -13
+game_over=False
 
 # Create bird
 bird = canvas.create_rectangle(bird_x, bird_y, bird_x + bird_size, bird_y + bird_size, fill="yellow")
 
+def jump(event):
+    global bird_speed_y
+    bird_speed_y=jump_strength
+    # root.after(40,move)
+
+def move():
+    global bird_y, bird_speed_y, score, game_over
+
+    # Apply gravity to the bird
+    bird_speed_y += gravity
+    bird_y += bird_speed_y
+    canvas.coords(bird, bird_x, bird_y, bird_x + bird_size, bird_y + bird_size)
+    if game_over==True:
+        canvas.create_text(WIDTH // 2, HEIGHT // 2, text="Game Over", font=('Arial', 24), fill="red")
+    else:
+        root.after(40,move)
+
+
+
+
+root.bind("<space>", jump)
+move()
 root.mainloop()
